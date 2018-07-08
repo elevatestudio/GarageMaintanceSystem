b939cc0b62eee5c35a0eb0065c89635a4e381fd1
sieeniu <pawelsieniewicz@gmail.com>
2018-07-08 21:59:07 +0200

    Plik indexu - w nim piszemy kod strony
    obsluga plików

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 var hostname = '127.0.0.1';
 var port = 8081;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+const server = http.createServer(function (request, result) {
+    fs.readFile('./document_root/index.html', function(err,html) {
+        result.writeHead(200, {'Content-Type': 'text/html'});
+        result.write(html);
+        result.end();
+    });
+
 });
 
 server.listen(port, hostname, () => {
