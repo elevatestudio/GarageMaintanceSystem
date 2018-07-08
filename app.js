680ca6f2b05e159d20438dfd6b6d7b5e5c7c7285
sieeniu <pawelsieniewicz@gmail.com>
2018-07-08 21:25:51 +0200

    Uruchomienie serwera
    Hostname = lokalny adres; Port 8080

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+var hostname = '127.0.0.1';
+var port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
